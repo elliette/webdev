d85d83b7ff81fbd3763fce9d7c9d8889f3cb0b58
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-11-18 11:04:52 -0800

    save changes

--- dwds/debug_extension_mv3/web/background.dart
+++ dwds/debug_extension_mv3/web/background.dart
@@ -88,6 +88,171 @@ void _handleRuntimeMessages(
       });
 }
 
+void _onDebuggerEvent(Debuggee source, String method, Object? params) {
+  if (method == 'Runtime.executionContextCreated') {
+    _handleExecutionContextCreated(source, params);
+  }
+
+  _forwardChromeDebuggerEventToDwds(source, method, params);
+}
+
+_onDebuggerAttached() {
+  chrome.debugger
+      .sendCommand(Debuggee(tabId: _tabId), 'Runtime.enable', EmptyParam(),
+          allowInterop((_) {
+    final chromeError = chrome.runtime.lastError;
+    if (chromeError != null) {
+      window.alert(_translateChromeError(chromeError.message));
+      return;
+    }
+  }));
+}
+
+void _forwardChromeDebuggerEventToDwds(
+    Debuggee source, String method, dynamic params) {
+  final debugSession = _debugSessions
+      .firstWhereOrNull((session) => session.appTabId == source.tabId);
+
+  if (debugSession == null) return;
+
+  final event = _extensionEventFor(method, params);
+
+  if (method == 'Debugger.scriptParsed') {
+    debugSession.sendBatchedEvent(event);
+  } else {
+    debugSession.sendEvent(event);
+  }
+}
+
+void _handleExecutionContextCreated(Debuggee source, Object? params) {
+  if (params == null) return;
+  final context = json.decode(JSON.stringify(params))['context'];
+  final contextOrigin = context['origin'] as String;
+  if (contextOrigin == _origin) {
+    final contextId = context['id'] as int;
+    _connectToDwds(contextId);
+  }
+}
+
+void _connectToDwds(int contextId) async {
+  final uri = Uri.parse(_extensionUri);
+  final authenticated = await _authenticateUser(uri, _tabId);
+  if (!authenticated) return;
+  final client = _createClient(uri);
+  _debugSessions.add(DebugSession(client, _tabId, _appId));
+
+  // Listen to events from DWDS:
+  client.stream.listen(
+    (data) => _routeDwdsEvent(data, client),
+    onDone: () {
+      console.log('Closing debug connection with DWDS.');
+      _removeAndDetachDebugSessionForTab(_tabId);
+    },
+    onError: (_) {
+      console.warn('Received error, closing debug connection with DWDS.');
+      _removeAndDetachDebugSessionForTab(_tabId);
+    },
+    cancelOnError: true,
+  );
+
+  _sendDevToolsRequest(client, contextId);
+}
+
+void _routeDwdsEvent(String eventData, SocketClient client) {
+  final message = serializers.deserialize(jsonDecode(eventData));
+  if (message is ExtensionRequest) {
+    _forwardDwdsEventToChromeDebugger(message, client);
+  } else if (message is ExtensionEvent) {
+    switch (message.method) {
+      case 'dwds.encodedUri':
+        // TODO(elliette): Forward to external extensions.
+        break;
+      case 'dwds.devtoolsUri':
+        _injectDevToolsIframe(message.params);
+        break;
+    }
+  }
+}
+
+void _forwardDwdsEventToChromeDebugger(
+    ExtensionRequest message, SocketClient client) {
+  final messageParams = message.commandParams ?? '{}';
+  final params = BuiltMap<String, Object>(json.decode(messageParams)).toMap();
+  chrome.debugger.sendCommand(
+      Debuggee(tabId: _tabId), message.command, js_util.jsify(params),
+      allowInterop(([e]) {
+    // No arguments indicate that an error occurred.
+    if (e == null) {
+      client.sink
+          .add(jsonEncode(serializers.serialize(ExtensionResponse((b) => b
+            ..id = message.id
+            ..success = false
+            ..result = JSON.stringify(chrome.runtime.lastError)))));
+    } else {
+      client.sink
+          .add(jsonEncode(serializers.serialize(ExtensionResponse((b) => b
+            ..id = message.id
+            ..success = true
+            ..result = JSON.stringify(e)))));
+    }
+  }));
+}
+
+void _sendDevToolsRequest(SocketClient client, int contextId) async {
+  final tabUrl = await _getTabUrl(_tabId);
+  client.sink.add(jsonEncode(serializers.serialize(DevToolsRequest((b) => b
+    ..appId = _appId
+    ..instanceId = _instanceId
+    ..contextId = contextId
+    ..tabUrl = tabUrl
+    ..uriOnly = true))));
+}
+
+Future<bool> _authenticateUser(Uri uri, int tabId) async {
+  var authUri = uri.replace(path: _authenticationPath);
+  if (authUri.scheme == 'ws') authUri = authUri.replace(scheme: 'http');
+  if (authUri.scheme == 'wss') authUri = authUri.replace(scheme: 'https');
+  final authUrl = authUri.toString();
+  try {
+    final response = await HttpRequest.request(authUrl,
+        method: 'GET', withCredentials: true);
+    final responseText = response.responseText ?? '';
+    if (!responseText.contains('Dart Debug Authentication Success!')) {
+      throw Exception('Not authenticated.');
+    }
+  } catch (_) {
+    if (window.confirm(
+        'Authentication required.\n\nClick OK to authenticate then try again.')) {
+      window.open(authUrl, 'Dart DevTools Authentication');
+      chrome.debugger.detach(Debuggee(tabId: tabId), allowInterop(() {}));
+    }
+    return false;
+  }
+  return true;
+}
+
+/// Construct an [ExtensionEvent] from [method] and [params].
+ExtensionEvent _extensionEventFor(String method, dynamic params) {
+  return ExtensionEvent((b) => b
+    ..params = jsonEncode(json.decode(JSON.stringify(params)))
+    ..method = jsonEncode(method));
+}
+
+SocketClient _createClient(Uri uri) {
+  if (uri.isScheme('ws') || uri.isScheme('wss')) {
+    return WebSocketClient(WebSocketChannel.connect(uri));
+  }
+  return SseSocketClient(SseClient(uri.toString()));
+}
+
+String _translateChromeError(String chromeErrorMessage) {
+  if (chromeErrorMessage.contains('Cannot access') ||
+      chromeErrorMessage.contains('Cannot attach')) {
+    return _notADartAppAlert;
+  }
+  return _devToolsAlreadyOpenedAlert;
+}
+
 Future<Tab?> _getTab() async {
   final query = QueryInfo(active: true, currentWindow: true);
   final tabs = List<Tab>.from(await promiseToFuture(chrome.tabs.query(query)));
