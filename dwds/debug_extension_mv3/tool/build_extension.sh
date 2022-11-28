32afc19fe8f826caf140e20fcd1dc3ee93b02780
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2022-11-28 12:15:54 -0800

    Debug logging wip

--- dwds/debug_extension_mv3/tool/build_extension.sh
+++ dwds/debug_extension_mv3/tool/build_extension.sh
@@ -9,12 +9,27 @@
 # Builds the unminifed dart2js app (see DDC issue: https://github.com/dart-lang/sdk/issues/49869):
 # ./tool/build_extension.sh
 
+# Builds the dart2js app for release:
+# ./tool/build_extension.sh prod
+
+prod="false"
+dev="true"
+
+case "$1" in
+    prod)
+        prod="true"
+        dev="false"
+        shift;;
+esac
+
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Building dart2js-compiled extension to /compiled directory."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-dart run build_runner build web --output build --release
+dart run build_runner build web --output build --release --define is_dev=$dev
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Updating manifest.json in /compiled directory."
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-dart tool/update_dev_files.dart
+if [ $prod == false ]; then
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "Updating dev files in /compiled directory."
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    dart tool/update_dev_files.dart
+fi
